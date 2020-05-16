d904d981698d7a9f431cd127d0e53b2a71c88a23
Vadim <guvacode@gmail.com>
2020-05-16 18:53:34 +0500

    small fix

--- package/pgf_descriprors.pas
+++ package/pgf_descriprors.pas
@@ -59,7 +59,16 @@ end;
 
 function TPGFFileUnit.GetInterfaceUsesSection: string;
 begin
-  {$I InterfaceUses.inc}
+  Result:=
+'SysUtils,'+LE+
+'  phxLogger,'+LE+
+'  phxTypes,'+LE+
+'  phxEvents,'+LE+
+'  phxMath,'+LE+
+'  phxApplication,'+LE+
+'  phxDevice,'+LE+
+'  phxCanvas,'+LE+
+'  phxTexture'
 end;
 
 function TPGFFileUnit.GetLocalizedName: string;
@@ -80,13 +89,84 @@ end;
 function TPGFFileUnit.GetImplementationSource(const Filename, SourceName,
   ResourceName: string): string;
 begin
-{$I ImplementationSource.inc}
+  Result:=
+  'uses'+LE+
+  ' phxOpenGL_GLFW3,'+LE+
+   {$IFDEF PHX_VAMPIRE}
+  ' phxGraphics_Vampyre;'+LE+
+   {$ELSE}
+  ' phxGraphics_FreeImage;'+LE+
+   {$ENDIF}
+  ''+LE+
+  'constructor TGame.Create;'+LE+
+  'begin'+LE+
+  '  inherited;'+LE+
+  ''+LE+
+  'end;'+LE+
+  ''+LE+
+  'procedure TGame.Init;'+LE+
+  'begin'+LE+
+  '  // Create the device using the default provider'+LE+
+  '  Device:= TPHXDevice.Create;'+LE+
+  '  // Initialize the device'+LE+
+  '  Device.Initialize(''Phoenix Demo'', 800, 600);'+LE+
+  '  // Create the canvas'+LE+
+  '  Canvas:= Device.CreateCanvas;'+LE+
+  '  // Create the timer'+LE+
+  '  Timer:= TPHXTimer.Create;'+LE+
+  'end;'+LE+
+  ''+LE+
+  'procedure TGame.Update;'+LE+
+  'begin'+LE+
+  '  Timer.Update;'+LE+
+  '  Device.Update;'+LE+
+  'end;'+LE+
+  ''+LE+
+  'procedure TGame.Render;'+LE+
+  'begin'+LE+
+  '  Device.Clear;'+LE+
+  '  Canvas.Flush;'+LE+
+  ''+LE+
+  '  Device.Flip;'+LE+
+  'end;'+LE+
+  ''+LE+
+  'procedure TGame.Shutdown;'+LE+
+  'begin'+LE+
+  '  Timer.Free;'+LE+
+  '  Canvas.Free;'+LE+
+  '  Device.Free;'+LE+
+  'end;'+LE+
+  ''+LE+
+  'procedure TGame.KeyPressed(Key: TPHXVirtualKey; Shift: TPHXShiftStates);'+LE+
+  'begin'+LE+
+  '  inherited;'+LE+
+  '  // Terminate the application with esc'+LE+
+  '  if Key = VK_ESC then'+LE+
+  '  begin'+LE+
+  '    Terminate;'+LE+
+  '  end;'+LE+
+  'end;'+LE+''+LE;
 end;
 
 function TPGFFileUnit.GetInterfaceSource(const aFilename, aSourceName,
   aResourceName: string): string;
 begin
- {$I interfacesource.inc}
+ Result:=
+'type'+LE+
+'TGame = class(TPHXApplication)'+LE+
+'  private'+LE+
+'    Device : TPHXDevice;'+LE+
+'    Timer  : TPHXTimer;'+LE+
+'    Canvas : TPHXCanvas;'+LE+
+'  protected'+LE+
+'    procedure KeyPressed(Key: TPHXVirtualKey; Shift: TPHXShiftStates); override;'+LE+
+'  public'+LE+
+'    constructor Create; override;'+LE+
+'    procedure Init; override;'+LE+
+'    procedure Update; override;'+LE+
+'    procedure Render; override;'+LE+
+'    procedure Shutdown; override;'+LE+
+'  end;'
 end;
 
 { TPGFApplicationDescriptor }
@@ -117,7 +197,7 @@ var
 begin
   Result:=inherited InitProject(AProject);
 
-  MainFile:=AProject.CreateProjectFile('myGame.lpr');
+  MainFile:=AProject.CreateProjectFile('Game.lpr');
   MainFile.IsPartOfProject:=true;
   AProject.AddFile(MainFile,false);
   AProject.MainFileID:=0;
@@ -126,7 +206,21 @@ begin
   AProject.LazCompilerOptions.SyntaxMode:='Delphi';
 
    // create program source
-  {$I programsource.inc}
+  NewSource:=
+  'program Game1;'+LE+
+   ''+LE+
+  'uses'+LE+
+  '   SysUtils;'+LE+
+  ''+LE+
+  ''+LE+
+  'var Game: TGame;'+LE+
+  ''+LE+
+  'begin'+LE+
+  '  Game:= TGame.Create;'+LE+
+  '  Game.Run;'+LE+
+  '  Game.Free;'+LE+
+  'end.'+LE;
+
   AProject.MainFile.SetSourceText(NewSource,true);
 
   AProject.AddPackageDependency('FCL');
